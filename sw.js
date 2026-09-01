6c131b63c65282494c3eb25f3fad5a25174c2a6e
Jomar71 <jorgenriquemartinezc@gmail.com>
2026-08-31 20:11:01 -0500

    chore: forzar actualizacion de cache v5.1.0 para eliminar letrero de conexion en todos los dispositivos

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Solo almacena en caché los archivos de la "shell" de la app;
    NO intercepta las peticiones al API para no interferir con la
    sincronización con el backend. */
-const CACHE = 'ayb-v1';
+const CACHE = 'ayb-v2';
 
 const SHELL = [
   './',
